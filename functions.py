605037120576049a79c2cb303a3483e138acb4bd
Igor Vremea <igori.vremya@gmail.com>
2024-03-29 19:46:45 +0200

    Database show v2.0 (easygui)

--- functions.py
+++ functions.py
@@ -1,3 +1,9 @@
-def show_list(list):
+def db_to_str(list):
+    str = ""
     for line in list:
-        print(f"{line[0]}.\t\b\b\b{line[1]} {line[2]}\t{line[3]}")
+        if line[4] == None:
+            email = "------"
+        else:
+            email = line[4]
+        str += f"{line[0]}. {line[1]} {line[2]}\t\t\t{line[3]}\t\t{email}\n"
+    return str
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from functions import *
 import sqlite3 as sl
+from easygui import *
 
 ######## FUNCTIONS ########
 #
 # show_list(list)
 #
 ###########################
-
 conn = sl.connect('ph_book.db')
 curs = conn.cursor()
 curs.execute("""
@@ -15,18 +15,18 @@ curs.execute("""
             name TEXT,
             surname TEXT,
             main_ph_num TEXT,
-            others_ph_nums TEXT,
             email TEXT
             )
             """)
 
 # curs.execute("INSERT INTO phone_book (name, surname, main_ph_num, email) VALUES ('Petea', 'Pupkin', '012345678', 'sobaka@mail.ru');")
-# curs.execute("INSERT INTO phone_book (name, surname, main_ph_num) VALUES ('Vasea', 'Pupkin', '012345678')")
-# curs.execute("INSERT INTO phone_book (name, surname, main_ph_num) VALUES ('Igor', 'Vremea', '876543210')")
-# conn.commit()
+# curs.execute("INSERT INTO phone_book (name, surname, main_ph_num) VALUES ('Vasea', 'Pupkin', '012345678');")
+# curs.execute("INSERT INTO phone_book (name, surname, main_ph_num) VALUES ('Igor', 'Vremea', '876543210');")
 curs.execute("SELECT * FROM phone_book")
-list = curs.fetchall()
-show_list(list)
+conn.commit()
+db_in_list = curs.fetchall()
+db_in_str = db_to_str(db_in_list)
+print(db_in_str)
 
-
+textbox('Contacts:\n\nName Surname\t\t\tPhone number\t\tEmail', 'Contacts', db_in_str)
 
