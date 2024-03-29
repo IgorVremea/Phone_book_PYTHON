d542a0c57501d1a36acec11b9a9dd494878c6ccc
Igor Vremea <igori.vremya@gmail.com>
2024-03-29 21:25:30 +0200

    Import v1.0

--- functions.py
+++
@@ -1,9 +0,0 @@
-def db_to_str(list):
-    str = ""
-    for line in list:
-        if line[4] == None:
-            email = "------"
-        else:
-            email = line[4]
-        str += f"{line[0]}. {line[1]} {line[2]}\t\t\t{line[3]}\t\t{email}\n"
-    return str
--- main.py
+++ main.py
@@ -21,7 +21,10 @@ def db_init():
                 email TEXT
                 )
                 """)
-    
+
+def window_menu(m_list):
+    return choicebox('Menu:', 'Menu', m_list)
+
 def window_show_pbook():
     curs.execute("SELECT * FROM phone_book")
     conn.commit()
@@ -30,9 +33,25 @@ def window_show_pbook():
     print(db_in_str)
     textbox('Contacts:\n\nName Surname\t\t\tPhone number\t\tEmail', 'Contacts', db_in_str)
 
-def window_menu(m_list):
-    return choicebox('Menu:', 'Menu', m_list)
-
+def window_import():
+    while 1:
+        import_path = fileopenbox('Choose a file ', 'Import', filetypes=["*.db"])
+        if import_path == None:
+            break
+        elif import_path[-3:] == ".db":
+            if import_path[-len(db_path):] == db_path:
+                msgbox('Эта база данных уже используется\n(Поробуйте переименовать файл)', 'WARNING')
+            else:
+                with open(db_path, 'w') as db, open(import_path, 'r') as import_file:
+                    for line in import_file:
+                        db.write(line)
+                conn = sl.connect(db_path)
+                curs = conn.cursor()
+                db_init()
+                msgbox("База данных была импортирована успешно!", "SUCCESS!")
+                break
+        elif import_path != None:
+            msgbox("AHTUNG!\nВыберите файл с расширением .db", "WARNING")
 
 def db_to_str(list):
     str = ""
@@ -46,21 +65,25 @@ def db_to_str(list):
 ################################################################################################
 ######################################## INITIALIZATION ########################################
 ################################################################################################
-conn = sl.connect('ph_book.db')
+db_path = 'ph_book.db'
+conn = sl.connect(db_path)
 curs = conn.cursor()
 
-menu_list = ['Exit', 'Show phone book', ]
-
-
-# curs.execute("INSERT INTO phone_book (name, surname, main_ph_num, email) VALUES ('Petea', 'Pupkin', '012345678', 'sobaka@mail.ru');")
-# curs.execute("INSERT INTO phone_book (name, surname, main_ph_num) VALUES ('Vasea', 'Pupkin', '012345678');")
-# curs.execute("INSERT INTO phone_book (name, surname, main_ph_num) VALUES ('Igor', 'Vremea', '876543210');")
-
+menu_list = ['Exit', 'Show phone book', 'Import phone book']
 
 ##############################################################################################
 ######################################## MAIN PROGRAM ########################################
 ##############################################################################################
 db_init()
+
+# curs.execute("INSERT INTO phone_book (name, surname, main_ph_num, email) VALUES ('Petea', 'Pupkin', '012345678', 'sobaka@mail.ru');")
+# curs.execute("INSERT INTO phone_book (name, surname, main_ph_num) VALUES ('Vasea', 'Pupkin', '012345678');")
+# curs.execute("INSERT INTO phone_book (name, surname, main_ph_num) VALUES ('Igor', 'Vremea', '876543210');")
+# curs.execute("INSERT INTO phone_book (name, surname, main_ph_num, email) VALUES ('Svinka', 'Pepa', '012345678', 'pios@mail.ru');")
+# curs.execute("INSERT INTO phone_book (name, surname, main_ph_num, email) VALUES ('Rick', 'Sanches', '012345678', '0_0@mail.net');")
+# curs.execute("INSERT INTO phone_book (name, surname, main_ph_num) VALUES ('Igor', 'Vremea', '876543210');")
+
+
 while 1:
     choice = window_menu(menu_list)
     print(choice)
@@ -69,4 +92,6 @@ while 1:
             break
         case 'Show phone book': 
             window_show_pbook()
+        case 'Import phone book':
+            window_import()
 
