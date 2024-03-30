30e31f0380d694b129b31e03ea38c8ca3e75d86f
Igor Vremea <igori.vremya@gmail.com>
2024-03-30 14:52:23 +0200

    Export v1.0

--- main.py
+++ main.py
@@ -53,6 +53,37 @@ def window_import():
         elif import_path != None:
             msgbox("AHTUNG!\nВыберите файл с расширением .db", "WARNING")
 
+def window_export():
+    while 1:
+        save_file_path = filesavebox('Выберите куда хотите экспортировать книгу:', 'SAVE')
+        print(save_file_path)
+        if save_file_path == None:
+            break
+        elif save_file_path[-3:] != '.db':
+            msgbox('AHTUNG!\nВыберите файл с разрешением .db', 'WARNING')
+            continue
+        else:
+            sf = open(save_file_path, 'w')
+            sf.close()
+            conn_exp = sl.connect(save_file_path)
+            curs_exp = conn_exp.cursor()
+            curs_exp.execute("""
+                CREATE TABLE IF NOT EXISTS phone_book(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT,
+                surname TEXT,
+                main_ph_num TEXT,
+                email TEXT
+                )
+                """)
+            db_in_list = db_to_list()
+            for i in db_in_list:
+                curs_exp.execute(f"INSERT INTO phone_book (name, surname, main_ph_num, email) VALUES ('{i[1]}', '{i[2]}', '{i[3]}', '{i[4]}')")
+            conn_exp.commit()
+            msgbox('✔️Экспорт успешно завершен', 'SUCCESS')
+            break
+
+
 def window_search():
     search = enterbox('Введите имя, фамилию, номер или почту контакта, полную информацию которого хотите найти:', 'SEARCH' )
     curs.execute(f"SELECT * FROM phone_book WHERE name = '{search}' OR surname = '{search}' OR main_ph_num = '{search}' OR email = '{search}' ORDER BY name,surname")
@@ -110,7 +141,7 @@ conn = sl.connect(db_path)
 curs = conn.cursor()
 db_in_list = []
 
-menu_list = ['Show phone book', 'Add new contact', 'Search contact', 'Change contact', 'Delete contact', 'Import phone book', 'Exit']
+menu_list = ['Show phone book', 'Add new contact', 'Search contact', 'Change contact', 'Delete contact', 'Import phone book', 'Export phone book', 'Exit']
 
 ##############################################################################################
 ######################################## MAIN PROGRAM ########################################
@@ -137,6 +168,8 @@ while 1:
             window_add()
         case 'Import phone book':
             window_import()
+        case 'Export phone book':
+            window_export()
         case 'Search contact':
             window_search()
         case 'Change contact':
